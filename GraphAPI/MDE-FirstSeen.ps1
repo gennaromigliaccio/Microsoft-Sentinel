2f55df3db42c24bf5253ebe4ce53e2423eb8ff62
gennaromigliaccio <51439087+gennaromigliaccio@users.noreply.github.com>
2025-10-03 10:31:31 +0100

    Create MDE-FirstSeen.ps1

---
+++ GraphAPI/MDE-FirstSeen.ps1
@@ -0,0 +1,34 @@
+# Define your app registration details
+$tenantId = "YOUR TENANT ID"
+$clientId = "CLIENT ID/REGISTERED APP ID"
+$clientSecret = "REGISTERED APP SECRET VALUE"
+
+
+ #Prepare token request
+$tokenUri = "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token"
+$tokenBody = "client_id=$clientId&scope=https%3A%2F%2Fapi.securitycenter.microsoft.com%2F.default&client_secret=$clientSecret&grant_type=client_credentials"
+
+# Get access token
+$tokenResponse = Invoke-RestMethod -Method Post -Uri $tokenUri -Body $tokenBody -ContentType "application/x-www-form-urlencoded"
+$accessToken = $tokenResponse.access_token
+
+# Prepare headers for Defender API call
+$headers = @{
+    Authorization = "Bearer $accessToken"
+}
+
+# Query Defender for device info
+$uri = "https://api.securitycenter.microsoft.com/api/machines"
+$response = Invoke-RestMethod -Method Get -Uri $uri -Headers $headers
+
+# Display device name and first seen date
+$response.value | Select-Object computerDnsName, firstSeen
+
+# Export to CSV
+
+$deviceData = $response.value | Select-Object computerDnsName, firstSeen
+
+$csvPath = "defender_devices.csv"
+$deviceData | Export-Csv -Path $csvPath -NoTypeInformation -Encoding UTF8
+
+Write-Host "Device data exported to $csvPath"
